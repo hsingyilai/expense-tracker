e610849dfcb9200ceeb8020eae75ee35174f6fdd
hsingyilai <hylaiphysics@gmail.com>
2026-07-23 18:35:07 -0700

    create the ExpenseEntry class which will replace the Expense class

--- expense_module.py
+++ expense_module.py
@@ -1,11 +1,22 @@
 class Expense:
-    def __init__(self, date, cost, category, note, quantity, tag):
+    '''An entry to the expense list
+
+    This class is the skeleton for recording an expense
+
+    Attributes:
+        date: A datetime.date object converted to string in standard ISO format, such as "2026-07-23".
+        cost: How much money does it cost in dollar.
+        category: What category does this expense belongs to.
+        note: Things that you want to take notes on according to the category it belongs to.
+        regular: True if it is a regular expense.
+    '''
+    def __init__(self, date: str, cost: float, category: str, note: dict, regular: bool):
         self.date = date
         self.cost = cost
         self.category = category
         self.note = note
-        self.quantity = quantity
-        self.tag = tag
+        self.regular = regular
+
 
 
 class Income:
@@ -14,3 +25,23 @@ class Income:
         self.amount = amount
         self.category = category
         self.note = note
+
+
+class ExpenseEntry:
+    '''An entry to the expense list
+
+    This class is the skeleton for recording an expense
+
+    Attributes:
+        date: A datetime.date object converted to string in standard ISO format, such as "2026-07-23".
+        cost: How much money does it cost in dollar.
+        category: What category does this expense belongs to.
+        note: Things that you want to take notes on according to the category it belongs to.
+        regular: True if it is a regular expense.
+    '''
+    def __init__(self, date: str, cost: float, category: str, note: dict, regular: bool):
+        self.date = date
+        self.cost = cost
+        self.category = category
+        self.note = note
+        self.regular = regular
