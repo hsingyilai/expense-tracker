4e0f1789e24deb74d7dae4f3d69b257529706ec7
hsingyilai <hylaiphysics@gmail.com>
2026-07-23 19:00:49 -0700

    code for converting expense list is done

---
+++ convert_to_new_class.py
@@ -0,0 +1,27 @@
+import json
+import datetime
+from expense_module import Income, Expense, ExpenseEntry
+
+
+with open("all_expense.json", "r") as f:
+    expense_list_data = json.load(f)
+
+expense_list = [Expense(**entry) for entry in expense_list_data]
+
+with open("irregular_expense_list.json", "r") as f:
+    irregular_list = json.load(f)
+
+updated_expense_list = []
+i = 0
+for entry in expense_list:
+    date = datetime.date(int(entry.date[2]), int(entry.date[0]), int(entry.date[1]))
+    note = {"note": entry.note, "quantity": entry.quantity, "tag": entry.tag}
+    updated_expense_list.append(
+        ExpenseEntry(str(date), entry.cost, entry.category, note, not irregular_list[i])
+    )
+    i += 1
+
+
+expense_list_data = [vars(entry) for entry in updated_expense_list]
+with open("my_expense.json", "w") as f:
+    json.dump(expense_list_data, f, indent=4)
--- expense_module.py
+++ expense_module.py
@@ -1,21 +1,11 @@
 class Expense:
-    '''An entry to the expense list
-
-    This class is the skeleton for recording an expense
-
-    Attributes:
-        date: A datetime.date object converted to string in standard ISO format, such as "2026-07-23".
-        cost: How much money does it cost in dollar.
-        category: What category does this expense belongs to.
-        note: Things that you want to take notes on according to the category it belongs to.
-        regular: True if it is a regular expense.
-    '''
-    def __init__(self, date: str, cost: float, category: str, note: dict, regular: bool):
+    def __init__(self, date, cost, category, note, quantity, tag):
         self.date = date
         self.cost = cost
         self.category = category
         self.note = note
-        self.regular = regular
+        self.quantity = quantity
+        self.tag = tag
 
 
 
